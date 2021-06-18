e3f3ebda85ec9e22220582f18e6ccc76be0fd2f7
Nick Kreeger <nickkreeger@microsoft.com>
2021-06-18 17:04:18 -0500

    save

--- onnxruntime/test/contrib_ops/embed_layer_norm_test_vectors.h
+++ onnxruntime/test/contrib_ops/embed_layer_norm_test_vectors.h
@@ -13,9 +13,9 @@ constexpr float kEpsilon = 1e-12f;
 
 // EmbedLayerNorm and QEmbedLayerNorm contain many inputs and outputs. This
 // utility class helps readability of Op unit tests by wrapping Op data.
-// TODO(kreeger): make this a struct if inheritance is not needed.
 class OpData {
  public:
+  // TODO - mark explicit?
   OpData(
       int batch_size,
       int sequence_size,
@@ -120,6 +120,190 @@ inline OpData EmbedLayerNormBatch1() {
                 gamma_data, beta_data, output_data, mask_index_data);
 }
 
+inline OpData EmbedLayerNormBatch2(bool has_mask=true) {
+  int batch_size = 3;
+  int sequence_size = 2;
+  int hidden_size = 4;
+
+  std::vector<int32_t> input_ids_data = {
+      1, 3,
+      1, 3,
+      2, 0};
+
+  std::vector<int32_t> segment_ids_data = {
+      0, 1,
+      0, 1,
+      0, 0};
+
+  std::vector<int32_t> mask_data = {};
+  if (has_mask) {
+    mask_data = {
+        1, 1,
+        1, 1,
+        1, 0};
+  }
+
+  std::vector<float> word_embedding_data = {
+      0.2f, 0.1f, 0.4f, -0.6f,
+      0.3f, 0.2f, 0.5f, 0.6f,
+      0.6f, 0.7f, 0.0f, -0.1f,
+      0.8f, 0.6f, 0.9f, 1.2f,
+      0.1f, 0.3f, 0.5f, 0.9f,
+      1.0f, -2.0f, 1.1f, 0.8f};
+
+  std::vector<float> position_embedding_data = {
+      0.1f, 0.1f, 0.4f, 0.6f,
+      0.6f, 0.0f, 0.8f, 0.6f,
+      0.3f, 0.9f, -2.0f, 0.8f};
+
+  std::vector<float> segment_embedding_data = {
+      0.3f, 0.4f, 0.9f, 0.1f,
+      0.7f, 0.3f, 0.5f, 0.2f};
+
+  std::vector<float> gamma_data = {
+      0.25f, 0.15f, 0.45f, -0.66f};
+
+  std::vector<float> beta_data = {
+      0.6f, 0.2f, 0.5f, -0.6f};
+
+  std::vector<float> output_data = {
+      0.36917170882225037, 0.061503000557422638, 1.1598974466323853, -0.85092413425445557,
+      0.74301940202713013, -0.057434864342212677, 0.84324657917022705, -0.85171419382095337,
+      0.36917170882225037, 0.061503000557422638, 1.1598974466323853, -0.85092413425445557,
+      0.74301940202713013, -0.057434864342212677, 0.84324657917022705, -0.85171419382095337,
+      0.57668739557266235, 0.2979130744934082, 0.96158987283706665, 0.44627034664154053,
+      0.64977931976318359, 0.11039737612009048, 1.1869535446166992, 0.14469735324382782};
+
+  std::vector<int32_t> mask_index_data = {
+      2, 2, 1};
+
+  return OpData(batch_size, sequence_size, hidden_size, input_ids_data, segment_ids_data,
+                mask_data, word_embedding_data, position_embedding_data, segment_embedding_data,
+                gamma_data, beta_data, output_data, mask_index_data, kEpsilon, has_mask);
+}
+
+inline OpData EmbedLayerNormLargeBatchSmallHiddenSize() {
+  int batch_size = 5;
+  int sequence_size = 2;
+  int hidden_size = 4;
+
+  std::vector<int32_t> input_ids_data = {
+      1, 3,
+      1, 3,
+      2, 0,
+      1, 3,
+      2, 0};
+
+  std::vector<int32_t> segment_ids_data = {
+      0, 1,
+      0, 1,
+      0, 0,
+      0, 1,
+      0, 0};
+
+  std::vector<int32_t> mask_data = {
+      1, 1,
+      1, 1,
+      1, 0,
+      1, 1,
+      1, 0};
+
+  std::vector<float> word_embedding_data = {
+      0.2f, 0.1f, 0.4f, -0.6f,
+      0.3f, 0.2f, 0.5f, 0.6f,
+      0.6f, 0.7f, 0.0f, -0.1f,
+      0.8f, 0.6f, 0.9f, 1.2f,
+      0.1f, 0.3f, 0.5f, 0.9f,
+      1.0f, -2.0f, 1.1f, 0.8f};
+
+  std::vector<float> position_embedding_data = {
+      0.1f, 0.1f, 0.4f, 0.6f,
+      0.6f, 0.0f, 0.8f, 0.6f,
+      0.3f, 0.9f, -2.0f, 0.8f};
+
+  std::vector<float> segment_embedding_data = {
+      0.3f, 0.4f, 0.9f, 0.1f,
+      0.7f, 0.3f, 0.5f, 0.2f};
+
+  std::vector<float> gamma_data = {
+      0.25f, 0.15f, 0.45f, -0.66f};
+
+  std::vector<float> beta_data = {
+      0.6f, 0.2f, 0.5f, -0.6f};
+
+  std::vector<float> output_data = {
+      0.36917170882225037, 0.061503000557422638, 1.1598974466323853, -0.85092413425445557,
+      0.74301940202713013, -0.057434864342212677, 0.84324657917022705, -0.85171419382095337,
+      0.36917170882225037, 0.061503000557422638, 1.1598974466323853, -0.85092413425445557,
+      0.74301940202713013, -0.057434864342212677, 0.84324657917022705, -0.85171419382095337,
+      0.57668739557266235, 0.2979130744934082, 0.96158987283706665, 0.44627034664154053,
+      0.64977931976318359, 0.11039737612009048, 1.1869535446166992, 0.14469735324382782,
+      0.36917170882225037, 0.061503000557422638, 1.1598974466323853, -0.85092413425445557,
+      0.74301940202713013, -0.057434864342212677, 0.84324657917022705, -0.85171419382095337,
+      0.57668739557266235, 0.2979130744934082, 0.96158987283706665, 0.44627034664154053,
+      0.64977931976318359, 0.11039737612009048, 1.1869535446166992, 0.14469735324382782};
+
+  std::vector<int32_t> mask_index_data = {
+      2, 2, 1, 2, 1};
+
+  return OpData(batch_size, sequence_size, hidden_size, input_ids_data, segment_ids_data,
+                mask_data, word_embedding_data, position_embedding_data, segment_embedding_data,
+                gamma_data, beta_data, output_data, mask_index_data);
+}
+
+inline OpData EmbedLayerNormBatch_Distill() {
+  int batch_size = 3;
+  int sequence_size = 2;
+  int hidden_size = 4;
+
+  std::vector<int32_t> input_ids_data = {
+      1, 3,
+      1, 3,
+      2, 0};
+
+  std::vector<int32_t> segment_ids_data = {};
+
+  std::vector<int32_t> mask_data = {
+      1, 1,
+      1, 1,
+      1, 0};
+
+  std::vector<float> word_embedding_data = {
+      0.2f, 0.1f, 0.4f, -0.6f,
+      0.3f, 0.2f, 0.5f, 0.6f,
+      0.6f, 0.7f, 0.0f, -0.1f,
+      0.8f, 0.6f, 0.9f, 1.2f,
+      0.1f, 0.3f, 0.5f, 0.9f,
+      1.0f, -2.0f, 1.1f, 0.8f};
+
+  std::vector<float> position_embedding_data = {
+      0.1f, 0.1f, 0.4f, 0.6f,
+      0.6f, 0.0f, 0.8f, 0.6f,
+      0.3f, 0.9f, -2.0f, 0.8f};
+
+  std::vector<float> segment_embedding_data = {};
+
+  std::vector<float> gamma_data = {
+      0.25f, 0.15f, 0.45f, -0.66f};
+
+  std::vector<float> beta_data = {
+      0.6f, 0.2f, 0.5f, -0.6f};
+
+  std::vector<float> output_data = {
+      0.39587587118148804, 0.03670068085193634, 0.7449488639831543, -1.4981462955474854,
+      0.61326867341995239, -0.046796366572380066, 0.81048583984375, -1.1954958438873291,
+      0.39587587118148804, 0.03670068085193634, 0.7449488639831543, -1.4981462955474854,
+      0.61326867341995239, -0.046796366572380066, 0.81048583984375, -1.1954958438873291,
+      0.75811392068862915, 0.38973665237426758, -0.069209933280944824, -0.18257927894592285,
+      0.73836749792098999, 0.071695566177368164, 1.111332893371582, 0.097372293472290039};
+
+  std::vector<int32_t> mask_index_data = {
+      2, 2, 1};
+
+  return OpData(batch_size, sequence_size, hidden_size, input_ids_data, segment_ids_data,
+                mask_data, word_embedding_data, position_embedding_data, segment_embedding_data,
+                gamma_data, beta_data, output_data, mask_index_data);
+}
 
 }  // namespace embedlayernorm
 }  // namespace test
